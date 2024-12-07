18d31e37973ba29564ddb7933cde0c7f5335ae57
gold-leo <168613134+gold-leo@users.noreply.github.com>
2024-12-06 19:52:47 -0600

    Implemented main

--- src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
+++ src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
@@ -1,6 +1,9 @@
 package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
+import java.util.function.*;
+
+import edu.grinnell.csc207.util.BrailleAsciiTables;
 
 /**
  *
@@ -15,7 +18,54 @@ public class BrailleASCII {
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    // STUB
+    if (args.length != 2) {
+      pen.println("Incorrect number of parameters.");
+      return;
+    } // if
+
+    String set;
+    StringBuilder result = new StringBuilder();
+    final Function<String, String> converter;
+
+    switch (args[0]) {
+      case "braille":
+        for (int i = 0; i < args[1].length(); i++) {
+          result.append(BrailleAsciiTables.toBraille(args[1].charAt(i)));
+        } // for
+        pen.println(result.toString());
+        return;
+
+      case "ascii":
+        converter = BrailleAsciiTables::toAscii;
+        set = args[1];
+        break;
+    
+      case "unicode":
+        StringBuilder intermediary = new StringBuilder();
+        for (int i = 0; i < args[1].length(); i++) {
+          intermediary.append(BrailleAsciiTables.toBraille(args[1].charAt(i)));
+        } // for
+        converter = BrailleAsciiTables::toUnicode;
+        set = intermediary.toString();
+        pen.println(set);
+        break;
+
+      default:
+        pen.println("Incorrect command.");
+        return;
+    } // switch
+
+    // Loop through the second arg's chars.
+    for (int i = 0; i < set.length(); i += 6) {
+      try {
+        result.append(converter.apply(set.substring(i, i+6)));
+      } catch (Exception e) {
+        pen.println("Improper conversion string.");
+        return;
+      } // try/catch
+    } // for
+
+    pen.print(result.toString());
     pen.close();
   } // main(String[]
 
